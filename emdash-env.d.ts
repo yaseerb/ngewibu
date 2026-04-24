51e4ec9d069ccfbc34e6fa64466bb123d172f11a
Yasir Bakhtiar <yasirbakhtiar252@gmail.com>
2026-04-24 15:47:22 +0700

    Update emdash-env.d.ts

--- emdash-env.d.ts
+++ emdash-env.d.ts
@@ -37,6 +37,18 @@ export interface Post {
   bylines?: ContentBylineCredit[];
 }
 
+export interface Anime {
+  id: string;
+  slug: string | null;
+  status: string;
+  title: string;
+  synopsis?: PortableTextBlock[];
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date | null;
+  bylines?: ContentBylineCredit[];
+}
+
 declare module "emdash" {
   interface EmDashCollections {
     pages: Page;
